c0fd8fd96b78c6b3ac05a41d753e351cb0746dde
Caleb <calebrod1@yahoo.com>
2023-01-25 14:04:27 -0600

    made labels for each drink in admin page

--- drinks/models.py
+++ drinks/models.py
@@ -3,4 +3,7 @@ from django.db import models
 class Drink(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
-    
+
+    # make name label for each drink in admin page:
+    def __str__(self):
+        return self.name + " " + self.description
