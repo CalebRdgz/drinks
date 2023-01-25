09e35acc5e16d66f34b76bac2fe7264d65353d0b
Caleb <calebrod1@yahoo.com>
2023-01-25 15:27:28 -0600

    added POST request for adding new data into my API

--- drinks/views.py
+++ drinks/views.py
@@ -2,12 +2,26 @@
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+# GET and POST request:
+@api_view(['GET', 'POST'])
 def drink_list(request):
-    # get all the drinks
-    drinks = Drink.objects.all()
-    # serialize the drinks
-    DrinkSerializer(drink_list, many=True) # use our DrinkSerializer class
-    serializer = DrinkSerializer(drinks, many=True) # create reference to the object^
-    # return JSON
-    return JsonResponse({"drinks": serializer.data}, safe=False)
+    # if GET request:
+    if request.method == 'GET':
+        # get all the drinks
+        drinks = Drink.objects.all()
+        # serialize the drinks
+        serializer = DrinkSerializer(drinks, many=True) # create reference to the object
+        # return JSON
+        return JsonResponse({"drinks": serializer.data})
+    # if POST request:
+    if request.method == 'POST':
+        # add a drink to database:
+        serializer = DrinkSerializer(data=request.data) # get data from the request, with reference "serializer"
+        # check if data is valid:
+        if serializer.is_valid(): 
+            serializer.save() # save the data if it is valid
+            return Response(serializer.data, status=status.HTTP_201_CREATED) # return a response with a status code
